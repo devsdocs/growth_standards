0a203a3782d30f178573e99ba6447873c21be410
devsdocs <id45go@gmail.com>
2025-06-12 04:38:27 +0900

    Add import for LMS model in base.dart to support velocity-based calculations

--- lib/src/common/base.dart
+++ lib/src/common/base.dart
@@ -1,4 +1,5 @@
 import 'package:growth_standards/growth_standards.dart';
+import 'package:growth_standards/src/common/model/lms.dart';
 
 abstract class VelocityBasedResult {
   const VelocityBasedResult();
