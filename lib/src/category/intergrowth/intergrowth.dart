e47012424d277a578dee10ffa4ffc160a49d7d79
devsdocs <id45go@gmail.com>
2025-06-14 00:38:55 +0900

    Refactor intergrowth.dart to reorganize and clean up import statements for better structure

--- lib/src/category/intergrowth/intergrowth.dart
+++ lib/src/category/intergrowth/intergrowth.dart
@@ -2,69 +2,61 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:growth_standards/growth_standards.dart';
 import 'package:growth_standards/src/common/extension.dart';
 import 'package:growth_standards/src/common/math.dart';
+import 'package:growth_standards/src/common/model/age.dart';
 import 'package:growth_standards/src/common/model/lms.dart';
 import 'package:growth_standards/src/common/tools.dart';
 import 'package:super_measurement/super_measurement.dart';
 
-part 'data/during_pregnancy/early_pregnancy_dating/grow_early_preg_charts_crl_dating.dart';
-part 'data/during_pregnancy/fetal_size_in_early_pregnancy/grow_early_preg_charts_size.dart';
-part 'data/during_pregnancy/symphysisfundal_height/grow_sfh_new.dart';
-part 'data/during_pregnancy/fetal_growth/grow_fetal_hc.dart';
-part 'data/during_pregnancy/fetal_growth/grow_fetal_bpd.dart';
-part 'data/during_pregnancy/fetal_growth/grow_fetal_ac.dart';
-part 'data/during_pregnancy/fetal_growth/grow_fetal_fl.dart';
-part 'data/during_pregnancy/fetal_growth/grow_fetal_ofd.dart';
-part 'data/during_pregnancy/gestational_weight_gain/grow_gwg_nw.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_boys_bw.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_girls_bw.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_boys_lt.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_girls_lt.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_boys_hc.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_girls_hc.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_wlr_boys.dart';
-part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_wlr_girls.dart';
 part 'data/at_birth/newborn_size/grow_newborn_boys_bw.dart';
-part 'data/at_birth/newborn_size/grow_newborn_girls_bw.dart';
-part 'data/at_birth/newborn_size/grow_newborn_boys_lt.dart';
-part 'data/at_birth/newborn_size/grow_newborn_girls_lt.dart';
 part 'data/at_birth/newborn_size/grow_newborn_boys_hc.dart';
+part 'data/at_birth/newborn_size/grow_newborn_boys_lt.dart';
+part 'data/at_birth/newborn_size/grow_newborn_girls_bw.dart';
 part 'data/at_birth/newborn_size/grow_newborn_girls_hc.dart';
+part 'data/at_birth/newborn_size/grow_newborn_girls_lt.dart';
 part 'data/at_birth/newborn_size/grow_newborn_wlr_boys.dart';
 part 'data/at_birth/newborn_size/grow_newborn_wlr_girls.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_boys_bw.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_boys_hc.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_boys_lt.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_girls_bw.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_girls_hc.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_girls_lt.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_wlr_boys.dart';
+part 'data/at_birth/newborn_size_for_very_preterm_infants/grow_verypreterm_wlr_girls.dart';
+part 'data/during_pregnancy/early_pregnancy_dating/grow_early_preg_charts_crl_dating.dart';
+part 'data/during_pregnancy/fetal_growth/grow_fetal_ac.dart';
+part 'data/during_pregnancy/fetal_growth/grow_fetal_bpd.dart';
+part 'data/during_pregnancy/fetal_growth/grow_fetal_fl.dart';
+part 'data/during_pregnancy/fetal_growth/grow_fetal_hc.dart';
+part 'data/during_pregnancy/fetal_growth/grow_fetal_ofd.dart';
+part 'data/during_pregnancy/fetal_size_in_early_pregnancy/grow_early_preg_charts_size.dart';
+part 'data/during_pregnancy/gestational_weight_gain/grow_gwg_nw.dart';
+part 'data/during_pregnancy/symphysisfundal_height/grow_sfh_new.dart';
 part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_boys_bw.dart';
-part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_girls_bw.dart';
-part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_boys_lt.dart';
-part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_girls_lt.dart';
 part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_boys_hc.dart';
+part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_boys_lt.dart';
+part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_girls_bw.dart';
 part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_girls_hc.dart';
-
-part 'model/at_birth/newborn_size/weight.dart';
+part 'data/postnatal/postnatal_growth_of_preterm_infants/grow_preterm_girls_lt.dart';
+part 'intergrowth.freezed.dart';
+part 'intergrowth.g.dart';
 part 'model/at_birth/newborn_size/head_circumference.dart';
 part 'model/at_birth/newborn_size/length.dart';
+part 'model/at_birth/newborn_size/weight.dart';
 part 'model/at_birth/newborn_size/weight_length_ratio.dart';
-
-part 'model/at_birth/newborn_size_for_very_preterm_infants/weight.dart';
 part 'model/at_birth/newborn_size_for_very_preterm_infants/head_circumference.dart';
 part 'model/at_birth/newborn_size_for_very_preterm_infants/length.dart';
+part 'model/at_birth/newborn_size_for_very_preterm_infants/weight.dart';
 part 'model/at_birth/newborn_size_for_very_preterm_infants/weight_length_ratio.dart';
-
 part 'model/during_pregnancy/early_pregnancy_dating/crl.dart';
-
 part 'model/during_pregnancy/fetal_growth/abdominal_circumference.dart';
 part 'model/during_pregnancy/fetal_growth/biparietal_diameter.dart';
-part 'model/during_pregnancy/fetal_growth/head_circumference.dart';
 part 'model/during_pregnancy/fetal_growth/femur_length.dart';
+part 'model/during_pregnancy/fetal_growth/head_circumference.dart';
 part 'model/during_pregnancy/fetal_growth/occipitofrontal_diameter.dart';
-
+part 'model/during_pregnancy/fetal_size_in_early_pregnancy/fetal_size_in_early_pregnancy.dart';
+part 'model/during_pregnancy/gestational_weight_gain/gestational_weight_gain.dart';
+part 'model/during_pregnancy/symphysisfundal_height/symphysisfundal_height.dart';
 part 'model/postnatal/postnatal_growth_of_preterm_infants/head_circumference.dart';
 part 'model/postnatal/postnatal_growth_of_preterm_infants/length.dart';
 part 'model/postnatal/postnatal_growth_of_preterm_infants/weight.dart';
-
-part 'model/during_pregnancy/symphysisfundal_height/symphysisfundal_height.dart';
-
-part 'model/during_pregnancy/gestational_weight_gain/gestational_weight_gain.dart';
-
-part 'model/during_pregnancy/fetal_size_in_early_pregnancy/fetal_size_in_early_pregnancy.dart';
-
-part 'intergrowth.freezed.dart';
-part 'intergrowth.g.dart';
